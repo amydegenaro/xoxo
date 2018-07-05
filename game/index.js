0f17b2cba4a80d9c0468a747a0f01bab227507a1
Amy De Genaro <amy.degenaro@gmail.com>
2018-07-05 12:40:51 -0400

    working board!

--- game/index.js
+++ game/index.js
@@ -1,6 +1,31 @@
 import {Map} from 'immutable'
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+const initialState = {
+  turn: 'X',
+  board: Map()
 }
+
+const MOVE = 'MOVE';
+
+const reducer = (state = initialState, action) => {
+  // TODO
+  switch(action.type) {
+    case MOVE:
+      return Object.assign({}, state, {
+        turn: action.player === 'X' ? 'O' : 'X',
+        board: state.board.setIn(action.position, action.player)
+      })
+    default:
+      return state
+  }
+}
+
+export const move = (player, position) => {
+  return {
+    type: MOVE,
+    position: position,
+    player: player
+  }
+}
+
+export default reducer
