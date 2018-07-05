353dc5d61b1aa2d86b2c164f2131ba540b7a68b0
Amy De Genaro <amy.degenaro@gmail.com>
2018-07-05 14:51:30 -0400

    updated reducer

--- game/index.js
+++ game/index.js
@@ -9,12 +9,16 @@ const MOVE = 'MOVE';
 
 const reducer = (state = initialState, action) => {
   // TODO
+  const newState = Object.assign({}, state);
   switch(action.type) {
     case MOVE:
-      return Object.assign({}, state, {
-        turn: action.player === 'X' ? 'O' : 'X',
-        board: state.board.setIn(action.position, action.player)
-      })
+      newState.board = newState.board.setIn(action.position, action.player);
+      newState.turn = action.player === 'X' ? 'O' : 'X';
+      return newState;
+      // return Object.assign({}, state, {
+      //   turn: action.player === 'X' ? 'O' : 'X',
+      //   board: state.board.setIn(action.position, action.player)
+      // })
     default:
       return state
   }
@@ -27,5 +31,28 @@ export const move = (player, position) => {
     player: player
   }
 }
+
+function winner(board) {
+  // use streak function to check coords for each possible win condition
+}
+
+function streak(board, firstCoord, ...remainingCoords) {
+  const player = board.getIn(firstCoord);
+
+  // grab coords from arguments without board
+  // check the coords passed in individually to see if they match or are undefined
+  // if they all match the first coord, return that player value
+  // otherwise return undefined
+
+
+  for (let r = 0; r != 3; r++) {
+    for (let c = 0; c != 3; c++) {
+      board.getIn([r,c])
+      if ()
+    }
+  }
+}
+
+
 
 export default reducer
